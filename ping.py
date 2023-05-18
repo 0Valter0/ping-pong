3bb7893842a2823f2f3c8380fbe245ce18c42690
0Valter0 <133788619+0Valter0@users.noreply.github.com>
2023-05-18 23:31:31 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,8 +1,7 @@
 from pygame import *
-pygame.init()
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_weight, player_hight, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_weight, player_hight, player_speed, player_speed_x, player_speed_y):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (player_weight, player_hight))
         self.rect = self.image.get_rect()
@@ -11,40 +10,46 @@ class GameSprite(sprite.Sprite):
         self.weight = player_weight
         self.hight = player_hight 
         self.speed = player_speed
+        self.speed_x = player_speed_x
+        self.speed_y = player_speed_y
+    def colliderect(self, rect):
+        return self.rect.colliderect(rect)
     def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
+        main_win.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
-            self.rect.x -= self.speed
-        if keys[K_s] and self.rect.y < 620:
-            self.rect.x += self.speed
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 395:
+            self.rect.y += self.speed
 
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
-            self.rect.x -= self.speed
-        if keys[K_DOWN] and self.rect.y < 620:
-            self.rect.x += self.speed
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 395:
+            self.rect.y += self.speed
 
 class Enemy(GameSprite):
-    if self.colliderect(platform_l.rect):
-        speed_x *= -1
-    if self.colliderect(platform_r.rect):
-        speed_x *= -1   
-    if self.rect.y > 450 or self.rect.y < 0:
-        speed_x *= -1
-    if self.rect.y < 0:
-        speed_y *= -1  
+    def update(self):
+        ball.rect.x += self.speed_x
+        ball.rect.y += self.speed_y
+        if self.colliderect(platform_l.rect):
+            self.speed_x *= -1
+        if self.colliderect(platform_r.rect):
+            self.speed_x *= -1  
+        if self.rect.y > 450 or self.rect.y < 0:
+            self.speed_y *= -1 
 
-platform_l = Player('platform.png', 50, 50, 20, 100, 10)
-platform_r = Player('platform.png', 50, 50, 20, 100, 10)
-ball = Enemy('ball.png', 350, 250, 50, 50, 5)
+platform_l = Player('platform_l.png', 50, 50, 20, 100, 10, 4, 4)
+platform_r = Player('platform_r.png', 650, 50, 20, 100, 10, 4, 4)
+ball = Enemy('ball.png', 200, 100, 50, 50, 5, 4, 4)
 win_width = 700
 win_height = 500
-back = transform.scale(image.load("background.jpeg"), (win_width, win_height)) 
+
+back = transform.scale(image.load("background.jpg"), (win_width, win_height)) 
 main_win = display.set_mode((win_width, win_height))
 clock = time.Clock()
 
@@ -59,12 +64,13 @@ while run:
         main_win.blit(back, (0, 0)) 
         
         platform_l.reset()
-        platform_l.update_l
+        platform_l.update_l()
 
         platform_r.reset()
-        platform_r.update_r
+        platform_r.update_r()
         
         ball.reset()
-        
+        ball.update()
+
         display.update()
     clock.tick(60)       
